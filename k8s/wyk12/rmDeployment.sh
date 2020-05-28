44568a33d97c9d113f311d6add4fc35f292549d8
Blazej <kucmeno12@o2.pl>
2020-05-28 07:58:15 +0200

    wyk12

---
+++ k8s/wyk12/rmDeployment.sh
@@ -0,0 +1,4 @@
+kubectl delete deployment backend-deploymnet --namespace=wyk12
+kubectl delete deployment postgres-deployment --namespace=wyk12
+kubectl delete deployment redis-deployment --namespace=wyk12
+kubectl delete configmap mayapp-config --namespace=wyk12
